c74c4d459e0fd8352e58dbb823dec3ff79845233
agnes1991(黄瑞) <rui_huang@outlook.com>
2016-12-26 15:06:19 +0800

    update

--- spider.py
+++ spider.py
@@ -1,16 +1,19 @@
+# coding:utf-8
+import bs4
 from bs4 import BeautifulSoup
 import requests
 
 url = 'https://www.oschina.net/blog'
 headers = {
-    "content_type":"text/html; charset=utf-8",
+    "content_type": "text/html; charset=utf-8",
     "User-Agent": "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
 }
 
 
 blog_data = requests.get(url, headers=headers)
+# print (blog_data.text)
+# blog_data.encoding = 'utf-8'
 soup = BeautifulSoup(blog_data.text, 'lxml')
-
 blog_titles = soup.select(".blog-list .item")
 
 for title in blog_titles:
@@ -18,5 +21,12 @@ for title in blog_titles:
     link = title.select(".blog-title-link")
     name = title.select(".blog-name")
     brief = title.select(".blog-brief")
-    print(link[0].attrs["href"], name[0].text, "=========", brief[0].text, "\n")
+    print (link[0].attrs["href"],
+           name[0].text,
+           "=========",
+           brief[0].text,
+           "\n")
+
+
+
 
