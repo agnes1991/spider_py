df66c66120481e5af15e67cd311d1a9925416a9c
agnes1991(黄瑞) <rui_huang@outlook.com>
2016-12-26 16:17:35 +0800

    update

--- spider.py
+++ spider.py
@@ -3,30 +3,35 @@ import bs4
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.oschina.net/blog'
-headers = {
-    "content_type": "text/html; charset=utf-8",
-    "User-Agent": "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
-}
+
+def blog(conn):
+    url = 'https://www.oschina.net/blog'
+    headers = {
+        "content_type": "text/html; charset=utf-8",
+        "User-Agent": "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
+    }
 
 
-blog_data = requests.get(url, headers=headers)
-# print (blog_data.text)
-# blog_data.encoding = 'utf-8'
-soup = BeautifulSoup(blog_data.text, 'lxml')
-blog_titles = soup.select(".blog-list .item")
+    blog_data = requests.get(url, headers=headers)
+    # print (blog_data.text)
+    # blog_data.encoding = 'utf-8'
+    soup = BeautifulSoup(blog_data.text, 'lxml')
+    blog_titles = soup.select(".blog-list .item")
 
-for title in blog_titles:
-    # for name in blog_name:
-    link = title.select(".blog-title-link")
-    name = title.select(".blog-name")
-    brief = title.select(".blog-brief")
-    print (link[0].attrs["href"],
-           name[0].text,
-           "=========",
-           brief[0].text,
-           "\n")
-
-
+    for title in blog_titles:
+        # for name in blog_name:
+        link = title.select(".blog-title-link")
+        name = title.select(".blog-name")
+        brief = title.select(".blog-brief")
+        # print (link[0].attrs['href'], name[0].text, '=========', brief[0].text, "\n")
+        print (link[0].attrs["href"])
+        print (name[0].text)
+        print (brief[0].text)
+        param = (name[0].text,
+                 brief[0].text,
+                 link[0].attrs["href"])
+        cursor = conn.cursor()
+        sql = 'insert into articles(title, content, url) value(%s, %s, %s)'
+        cursor.execute(sql, param)
 
 
--- storage.py
+++ storage.py
@@ -3,12 +3,14 @@
 import MySQLdb
 import spider
 
-conn = MySQLdb.connect(host='localhost', user='root', passwd='admin', db='spider_py', charset='utf-8')
+
+
+conn = MySQLdb.connect(host="localhost", user="root", passwd="admin", db="spider_py", charset="utf8")
 conn.autocommit(True)
 cursor = conn.cursor()
-
+spider.blog(conn)
 # 插入获取的数据
-sql = 'insert into artciles(title, content, url) value(%s, %s, %s)'
+# sql = 'insert into artciles(link, name, brief) value(%s, %s, %s)'
 
 
 
